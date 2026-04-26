d04812d824e7c43051f5d96ef6a0c07e08914743
willis dreee <xiuyyh@gmail.com>
2026-04-26 11:46:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-534017095-a33ca",
+  "appId": "1:731667670429:web:fe4ae943ea44c553f83b74",
+  "apiKey": "AIzaSyAMzfA428_YPw_nBaqrz_nl0jYg_WP4K_s",
+  "authDomain": "studio-534017095-a33ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "731667670429"
+};
